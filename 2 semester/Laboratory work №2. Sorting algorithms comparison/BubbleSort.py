5a70d852bb21608d67080033ac0c2adc9243914a
Roman Troshin <R.R.Troshin@outlook.com>
2021-04-13 22:10:41 +0300

    Program SortingAlgorithmsComparison 1.6
    Fix function bubbleSort from file BubbleSort.py

--- 2 semester/Laboratory work №2. Sorting algorithms comparison/BubbleSort.py
+++ 2 semester/Laboratory work №2. Sorting algorithms comparison/BubbleSort.py
@@ -1,6 +1,6 @@
 def bubbleSort(arr):
     for i in range(len(arr) - 1):
         for j in range(len(arr) - i - 1):
-            if arr[j] < arr[j + 1]:
+            if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
