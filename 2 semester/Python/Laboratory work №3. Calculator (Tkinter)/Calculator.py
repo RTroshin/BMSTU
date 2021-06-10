26ba55a7be52a3ad6f84381e26661ca047c2b8f6
Roman Troshin <R.R.Troshin@outlook.com>
2021-06-10 22:30:45 +0300

    Program Calculator 1.2
    Update file Calculator.py

--- 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
+++ 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
@@ -234,7 +234,6 @@ def main():
                  font=('Roboto', 32),
                  width=15)
 
-    calc = Entry(root, justify=RIGHT, font=('Roboto', 32), width=15)
     # calc = Entry(root, justify=RIGHT, font=('Roboto Black', 32), width=15)
     # calc = Entry(root, justify=RIGHT, font=('Monserat ExtraBold', 32), width=15)
     # calc = Entry(root, justify=RIGHT, font=('Play', 32))
