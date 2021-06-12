5f5e472a852bc05f8a12e5f48dae7d4df2b50a32
Roman Troshin <R.R.Troshin@outlook.com>
2021-06-12 23:50:59 +0300

    Program Calculator 1.2
    Update file Calculator.py

--- 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
+++ 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
@@ -263,7 +263,12 @@ def main():
                                     padx=1,
                                     pady=1)
 
-    makeDigitButton(calc, '3').grid(row=2, column=2, stick='wens', padx=1, pady=1)
+    makeDigitButton(calc, '3').grid(row=4,
+                                    column=2,
+                                    stick='wens',
+                                    padx=1,
+                                    pady=1)
+
     makeDigitButton(calc, '4').grid(row=3, column=0, stick='wens', padx=1, pady=1)
     makeDigitButton(calc, '5').grid(row=3, column=1, stick='wens', padx=1, pady=1)
     makeDigitButton(calc, '6').grid(row=3, column=2, stick='wens', padx=1, pady=1)
