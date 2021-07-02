a3ba85d2e3da80f6b46302aa7f2bcc8a01b7149f
Roman Troshin <R.R.Troshin@outlook.com>
2021-07-02 23:45:51 +0300

    Program Calculator 1.4
    Update file Calculator.py

--- 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
+++ 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
@@ -201,7 +201,7 @@ def main():
                      tearoff=0,
                      bg='#222222',
                      fg='#EEEEEE',
-                     activebackground='#444444')
+                     activebackground='#333333')
 
     mainMenu.add_cascade(label='Вид',
                          menu=firstItem)
