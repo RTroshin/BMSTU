42b5f4015c2a6b84f514c28bb40f061f64792dd9
Roman Troshin <R.R.Troshin@outlook.com>
2021-07-07 23:05:08 +0300

    Program Calculator 1.4
    Update file Calculator.py

--- 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
+++ 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
@@ -386,7 +386,7 @@ def main():
     root.grid_columnconfigure(2, minsize=100)
     root.grid_columnconfigure(3, minsize=100)
 
-    root.grid_rowconfigure(0, minsize=40)
+    root.grid_rowconfigure(0, minsize=10)
     root.grid_rowconfigure(1, minsize=80)
     root.grid_rowconfigure(2, minsize=80)
     root.grid_rowconfigure(3, minsize=80)
