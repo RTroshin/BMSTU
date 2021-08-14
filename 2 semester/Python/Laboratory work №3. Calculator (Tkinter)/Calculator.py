b3fc3a875a5a4e5d8940ae4c50c85787d7f6092b
Roman Troshin <R.R.Troshin@outlook.com>
2021-08-14 23:30:20 +0300

    Program Calculator 1.6
    Update file Calculator.py

--- 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
+++ 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
@@ -470,11 +470,14 @@ def main():
     button0.bind("<Enter>", lambda event : on_enter_digit(button0, event))
     button0.bind("<Leave>", lambda event : on_leave_digit(button0, event))
 
-    makeOperationButton(calc, calcHistory, '±').grid(row=7,
-                                                     column=0,
-                                                     stick='wens',
-                                                     padx=1,
-                                                     pady=1)
+    opButton = makeOperationButton(calc, calcHistory, '±')
+    opButton.grid(row=8,
+                  column=0,
+                  stick='wens',
+                  padx=1,
+                  pady=1)
+    opButton.bind("<Enter>", lambda event : on_enter_operation(opButton, event))
+    opButton.bind("<Leave>", lambda event : on_leave_operation(opButton, event))
 
     makeOperationButton(calc, calcHistory, ',').grid(row=7,
                                                      column=2,
