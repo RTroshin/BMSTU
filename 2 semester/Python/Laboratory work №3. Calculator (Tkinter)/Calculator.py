49a5396b240c7275e9c6360d41eddbd35807f3f9
Roman Troshin <R.R.Troshin@outlook.com>
2021-08-17 23:50:06 +0300

    Program Calculator 1.6
    Update file Calculator.py

--- 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
+++ 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
@@ -533,11 +533,14 @@ def main():
     opButton12.bind("<Enter>", lambda event : on_enter_calculate(opButton12, event))
     opButton12.bind("<Leave>", lambda event : on_leave_calculate(opButton12, event))
 
-    makeClearButton(calc, calcHistory, 'CE').grid(row=3,
-                                                  column=0,
-                                                  stick='wens',
-                                                  padx=1,
-                                                  pady=1)
+    opButton5 = makeClearButton(calc, calcHistory, 'CE')
+    opButton5.grid(row=4,
+                   column=0,
+                   stick='wens',
+                   padx=1,
+                   pady=1)
+    opButton5.bind("<Enter>", lambda event : on_enter_operation(opButton5, event))
+    opButton5.bind("<Leave>", lambda event : on_leave_operation(opButton5, event))
 
     makeClearButton(calc, calcHistory, 'C').grid(row=3,
                                                  column=1,
