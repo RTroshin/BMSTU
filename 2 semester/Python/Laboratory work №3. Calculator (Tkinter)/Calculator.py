2cf7e948f0f1d79641e099ee912f49788a1b608a
Roman Troshin <R.R.Troshin@outlook.com>
2021-08-27 23:45:08 +0300

    Program Calculator 1.7
    Update file Calculator.py

--- 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
+++ 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
@@ -262,23 +262,6 @@ def clearCE(calc):
         calc['state'] = DISABLED
 
 
-# # Очистка поля ввода
-# def clear(calc, calcHistory):
-#     global block
-#     block= False
-
-#     setDefaultSettings(calc)
-#     calcHistory['state'] = NORMAL
-#     calcHistory.delete(0, END)
-#     calcHistory.insert(0, '')
-#     calcHistory['state'] = DISABLED
-
-#     calc['state'] = NORMAL
-#     calc.delete(0, END)
-#     calc.insert(0, 0)
-#     calc['state'] = DISABLED
-
-
 # Ограничение ввода с клавиатуры. Только цифры и знаки операций
 def pressKey(calc, event):
 
