52e6b95971c00779b6f6faabf6683d154a39e371
Roman Troshin <R.R.Troshin@outlook.com>
2021-09-13 21:35:20 +0300

    Program Calculator 1.9
    Update file Calculator.py

--- 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
+++ 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
@@ -495,6 +495,41 @@ def digit(num):
         return 'F'
 
 
+def digitReturn(num):
+    if num == '0':
+        return '0'
+    elif num == '1':
+        return '1'
+    elif num == '2':
+        return '2'
+    elif num == '3':
+        return '3'
+    elif num == '4':
+        return '4'
+    elif num == '5':
+        return '5'
+    elif num == '6':
+        return '6'
+    elif num == '7':
+        return '7'
+    elif num == '8':
+        return '8'
+    elif num == '9':
+        return '9'
+    elif num == 'A':
+        return '10'
+    elif num == 'B':
+        return '11'
+    elif num == 'C':
+        return '12'
+    elif num == 'D':
+        return '13'
+    elif num == 'E':
+        return '14'
+    elif num == 'F':
+        return '15'
+
+
 # Установка настроек по-умолчанию
 # Необходима после вывода сообщений об ошибках
 def setDefaultSettings(calc):
