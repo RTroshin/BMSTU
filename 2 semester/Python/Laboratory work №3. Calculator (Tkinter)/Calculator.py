caa8740bd31dae029c7e71742a0a84306cebad3f
Roman Troshin <R.R.Troshin@outlook.com>
2021-09-23 23:00:48 +0300

    Program Calculator 2.0
    Update file Calculator.py

--- 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
+++ 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
@@ -775,7 +775,7 @@ def main():
     button9.bind("<Enter>", lambda event : on_enter_digit(button9, event))
     button9.bind("<Leave>", lambda event : on_leave_digit(button9, event))
 
-    button0 = makeDigitButton(calc, '0')
+    button0 = makeDigitButton(calc, calcHistory, '0')
     button0.grid(row=8,
                  column=1,
                  stick='wens',
