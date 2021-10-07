404918215cce54ac100b9af20aea2b6b87c5a84f
Roman Troshin <R.R.Troshin@outlook.com>
2021-10-07 16:20:08 +0300

    Program Calculator 2.0
    Update file Calculator.py

--- 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
+++ 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
@@ -74,17 +74,6 @@ from Functions import *
 from NumericalSystemFunctions import *
 
 
-# Замена декоративных знаков в строке
-def replaceSymbol(value):
-    if '–' in value:
-        value = value.replace('–', '-')
-    elif '×' in value:
-        value = value.replace('×', '*')
-    elif '÷' in value:
-        value = value.replace('÷', '/')
-    return value
-
-
 # Замена обычных знаков в строке на декоративных
 def replaceSymbolReturn(value):
     if '-' in value:
