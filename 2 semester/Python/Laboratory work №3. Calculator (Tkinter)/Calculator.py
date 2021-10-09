89cdbb69f7227f6d4193b432e95223a6c5056481
Roman Troshin <R.R.Troshin@outlook.com>
2021-10-09 22:10:10 +0300

    Program Calculator 2.0
    Update file Calculator.py

--- 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
+++ 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/Calculator.py
@@ -74,16 +74,6 @@ from Functions import *
 from NumericalSystemFunctions import *
 
 
-# Очистка поля ввода по кнопке CE
-def clearCE(calc):
-    global block
-    if (block != True):
-        calc['state'] = NORMAL
-        calc.delete(0, END)
-        calc.insert(0, 0)
-        calc['state'] = DISABLED
-
-
 # Очистка поля ввода по кнопке C
 def clearC(calc, calcHistory):
     global block
