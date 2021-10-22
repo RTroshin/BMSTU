37129697fac47dfa1721bcd6b050326cee874afd
Roman Troshin <R.R.Troshin@outlook.com>
2021-10-22 20:40:21 +0300

    Program Calculator 2.0
    Add new file NumericalSystemFunctions.py

---
+++ 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/NumericalSystemFunctions.py
@@ -0,0 +1,235 @@
+from tkinter.constants import RIGHT, END, NORMAL, DISABLED
+
+from Calculator import *
+from Functions import *
+from Buttons import *
+
+
+block = False # Глобальная переменная
+
+
+# Перевод из десятичной в двоичную систему счисления
+def decToBin(calc):
+    value = float(calc.get())
+    valueInt = int(value)
+    valueFloat = value - int(value)
+
+    if valueInt == 0:
+        return '0'
+
+    result = ''
+    while valueInt > 0:
+        result = str(valueInt % 2) + result
+        valueInt //= 2
+
+    if valueFloat:
+        result += '.'
+    else:
+        result += ''
+
+    iter = 0
+    while valueFloat > 1e-5 and iter < 5:
+        valueFloat = valueFloat * 2
+        number = (int)(valueFloat)
+        result += str(number)
+        valueFloat -= int(valueFloat)
+        iter += 1
+
+    calc['state'] = NORMAL
+    calc.delete(0, END)
+    calc.insert(0, result)
+    calc['state'] = DISABLED
+
+
+# Перевод из десятичной в восьмеричную систему счисления
+def decToOct(calc):
+    value = float(calc.get())
+    valueInt = int(value)
+    valueFloat = value - int(value)
+
+    if valueInt == 0:
+        return '0'
+
+    result = ''
+    while valueInt > 0:
+        result = str(valueInt % 8) + result
+        valueInt //= 8
+
+    if valueFloat:
+        result += '.'
+    else:
+        result += ''
+
+    iter = 0
+    while valueFloat > 1e-5 and iter < 5:
+        valueFloat = valueFloat * 8
+        number = (int)(valueFloat)
+        result += str(number)
+        valueFloat -= int(valueFloat)
+        iter += 1
+
+    calc['state'] = NORMAL
+    calc.delete(0, END)
+    calc.insert(0, result)
+    calc['state'] = DISABLED
+
+
+# Перевод из десятичной в десятичную систему счисления
+def returnToDec(calc):
+    value = float(calc.get())
+    valueInt = int(value)
+    valueFloat = value - int(value)
+
+    if valueInt == 0:
+        return '0'
+
+    resultInt = 0
+    i = 0
+    while valueInt > 0:
+        resultInt += pow(2 * valueInt % 10, i)
+        valueInt = int(valueInt / 10)
+        i += 1
+
+    resultInt = str(resultInt)
+    if valueFloat:
+        resultInt += '.'
+    # else:
+    #     result += ''
+
+    # resultFloat = 0
+    # valueFloat = str(valueFloat)
+    # print("valueFloat = ", valueFloat)
+    # j = 1
+    # for i in range(len(valueFloat)):
+    #     print("valueFloat[i] = ", valueFloat[i])
+    #     if i > 1:
+    #         resultFloat +=  int(valueFloat[i]) * pow(2, -j)
+    #         print("resultFloat = ", resultFloat)
+    #         print("j = ", j)
+    #         j += 1
+        
+    # resultFloat = float('0.' + str(resultFloat))
+
+    # resultFloat = 0
+    # print(valueFloat)
+    # valueFloat *= 1e+5
+    # valueFloat = int(valueFloat)
+    # print(valueFloat)
+    # i = len(str(valueFloat))
+    # while valueFloat > 0:
+    #     resultFloat += pow(2 * valueFloat % 10, -i)
+    #     valueFloat = int(valueFloat / 10)
+    #     i -= 1
+
+    # print(resultFloat)
+
+    calc['state'] = NORMAL
+    calc.delete(0, END)
+    # calc.insert(0, str(float(resultInt) + resultFloat))
+    calc.insert(0, str(resultInt))
+    calc['state'] = DISABLED
+
+
+# Перевод из десятичной в шестнадцатиричную систему счисления
+def decToHex(calc):
+    value = float(calc.get())
+    valueInt = int(value)
+    valueFloat = value - int(value)
+
+    if valueInt == 0:
+        return '0'
+
+    result = ''
+    while valueInt > 0:
+        result = str(digit(valueInt % 16)) + result
+        valueInt //= 16
+
+    if valueFloat:
+        result += '.'
+    else:
+        result += ''
+
+    iter = 0
+    while valueFloat > 1e-5 and iter < 5:
+        valueFloat = valueFloat * 16
+        number = (int)(valueFloat)
+        result += str(digit(number))
+        valueFloat -= int(valueFloat)
+        iter += 1
+
+    calc['state'] = NORMAL
+    calc.delete(0, END)
+    calc.insert(0, result)
+    calc['state'] = DISABLED
+
+
+# Преобразование десятичных чисел в шестнадцатиричные
+def digit(num):
+    if num == 0:
+        return '0'
+    elif num == 1:
+        return '1'
+    elif num == 2:
+        return '2'
+    elif num == 3:
+        return '3'
+    elif num == 4:
+        return '4'
+    elif num == 5:
+        return '5'
+    elif num == 6:
+        return '6'
+    elif num == 7:
+        return '7'
+    elif num == 8:
+        return '8'
+    elif num == 9:
+        return '9'
+    elif num == 10:
+        return 'A'
+    elif num == 11:
+        return 'B'
+    elif num == 12:
+        return 'C'
+    elif num == 13:
+        return 'D'
+    elif num == 14:
+        return 'E'
+    elif num == 15:
+        return 'F'
+
+
+# Преобразование шестнадцатиричных чисел в десятичные
+def digitReturn(num):
+    if num == '0':
+        return '0'
+    elif num == '1':
+        return '1'
+    elif num == '2':
+        return '2'
+    elif num == '3':
+        return '3'
+    elif num == '4':
+        return '4'
+    elif num == '5':
+        return '5'
+    elif num == '6':
+        return '6'
+    elif num == '7':
+        return '7'
+    elif num == '8':
+        return '8'
+    elif num == '9':
+        return '9'
+    elif num == 'A':
+        return '10'
+    elif num == 'B':
+        return '11'
+    elif num == 'C':
+        return '12'
+    elif num == 'D':
+        return '13'
+    elif num == 'E':
+        return '14'
+    elif num == 'F':
+        return '15'
