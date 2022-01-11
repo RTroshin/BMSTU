3fe7018656f636cd2a673d6f24d00467480d60d2
Roman Troshin <R.R.Troshin@outlook.com>
2022-01-11 22:25:14 +0300

    Program Calculator 2.1
    Update file NumericalSystemFunctions.py

--- 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/NumericalSystemFunctions.py
+++ 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/NumericalSystemFunctions.py
@@ -54,6 +54,15 @@ def decToBin(calc):
         calc['state'] = DISABLED
 
 
+# Перевод из десятичной в троичную систему счисления
+def decToThird(calc):
+    global BLOCK
+    global SYSNUMBER
+    if (BLOCK != True):
+        if SYSNUMBER == 10:
+            SYSNUMBER = 3
+
+
 # Перевод из десятичной в восьмеричную систему счисления
 def decToOct(calc):
     value = float(calc.get())
