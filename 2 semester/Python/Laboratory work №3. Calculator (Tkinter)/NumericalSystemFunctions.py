7b6ebdc499a243fd9a1bf441a9a66f0718944a4f
Roman Troshin <R.R.Troshin@outlook.com>
2022-01-22 23:25:54 +0300

    Program Calculator 2.1
    Update file NumericalSystemFunctions.py

--- 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/NumericalSystemFunctions.py
+++ 2 semester/Python/Laboratory work №3. Calculator (Tkinter)/NumericalSystemFunctions.py
@@ -116,6 +116,9 @@ def decToFour(calc):
         valueInt = int(value)
         valueFloat = value - int(value)
 
+        if valueInt == 0:
+            return '0'
+
 
 # Перевод из десятичной в восьмеричную систему счисления
 def decToOct(calc):
