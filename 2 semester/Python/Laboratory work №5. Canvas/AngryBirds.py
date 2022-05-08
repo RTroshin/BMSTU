d9790935c73f60223791decf78486264b57b6699
Roman Troshin <R.R.Troshin@outlook.com>
2022-05-08 22:15:48 +0300

    Program AngryBirds 1.1
    Update file AngryBirds.py

--- 2 semester/Python/Laboratory work №5. Canvas/AngryBirds.py
+++ 2 semester/Python/Laboratory work №5. Canvas/AngryBirds.py
@@ -84,35 +84,6 @@ c.create_arc((230, 110), (370, 190),
              outline='black',
              width=10)
 
-# Волосы на макушке
-# c.create_arc((195, 60), (280, 160), 
-#              start=0,
-#              extent=40, 
-#              style=ARC,
-#              outline='black',
-#              width=23)
-# c.create_oval(255, 65, 280, 85, 
-#               fill='black',
-#               outline='black')
-# c.create_arc((263, 45), (303, 175), 
-#              start=0,
-#              extent=70, 
-#              style=ARC,
-#              outline='black',
-#              width=33)
-# c.create_oval(272, 31, 303, 62, 
-#               fill='black',
-#               outline='black')
-# c.create_arc((320, 55), (420, 165), 
-#              start=180,
-#              extent=-60, 
-#              style=ARC,
-#              outline='black',
-#              width=36)
-# c.create_oval(324, 45, 362, 79, 
-#               fill='black',
-#               outline='black')
-
 # КЛЮВ
 ##############################################################################
 
